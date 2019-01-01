fe06e22f9e5e855d97b57af773fd2617b90b5a3d
Udit Karode <udit.karode@gmail.com>
2019-01-02 01:56:36 +0530

    gradle: add androidx.recyclerview dependency
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

---
+++ app/src/main/java/io/github/uditkarode/ididit/ResolutionAdapter.kt
@@ -0,0 +1,25 @@
+package io.github.uditkarode.ididit
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+
+/*class ResolutionAdapter: RecyclerView.Adapter<ResolutionAdapter.ResolutionV>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResolutionV {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: ResolutionV, position: Int) {
+
+    }
+
+
+    inner class ResolutionV(@NonNull itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+}*/
