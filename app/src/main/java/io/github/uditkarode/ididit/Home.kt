30a4e71dae55f6c73bb9e90921e5287654bdf03d
Udit Karode <udit.karode@gmail.com>
2019-01-01 12:53:12 +0530

    IDI: add Home class
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

---
+++ app/src/main/java/io/github/uditkarode/ididit/Home.kt
@@ -0,0 +1,12 @@
+package io.github.uditkarode.ididit
+
+import android.app.Activity
+import android.os.Bundle
+
+class Home : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
