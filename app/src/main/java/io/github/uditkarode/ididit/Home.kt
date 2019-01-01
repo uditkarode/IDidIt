72b3d3ff9ff12b1feb9d62f41181f927c14ae0f7
Udit Karode <udit.karode@gmail.com>
2019-01-02 01:56:36 +0530

    gradle: add androidx.recyclerview dependency
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/Home.kt
+++ app/src/main/java/io/github/uditkarode/ididit/Home.kt
@@ -1,12 +1,44 @@
 package io.github.uditkarode.ididit
 
-import android.app.Activity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import co.revely.gradient.RevelyGradient
+import io.github.uditkarode.ididit.utils.BABDrawer
+import kotlinx.android.synthetic.main.activity_home.*
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
-class Home : Activity() {
+class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(bottombar)
 
+        RevelyGradient.linear()
+            .colors(intArrayOf(Color.parseColor("#ddd6f3"), Color.parseColor("#faaca8")))
+            .on(homeheader)
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BABDrawer()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+        }
+        return true
     }
 }
