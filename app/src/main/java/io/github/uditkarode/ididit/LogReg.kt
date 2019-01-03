c9a8a9362900d74ea47d97cb349c10ccdc15a456
Udit Karode <udit.karode@gmail.com>
2019-01-03 16:05:08 +0530

    LogReg: Obtain gradient colors from Constants.kt, extend Activity
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/LogReg.kt
+++ app/src/main/java/io/github/uditkarode/ididit/LogReg.kt
@@ -1,22 +1,23 @@
 package io.github.uditkarode.ididit
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.revely.gradient.RevelyGradient
+import io.github.uditkarode.ididit.utils.Constants
 import kotlinx.android.synthetic.main.activity_logreg.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
-class LogReg : AppCompatActivity() {
+class LogReg : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logreg)
 
         RevelyGradient.linear()
-            .colors(intArrayOf(Color.parseColor("#70e1f5"), Color.parseColor("#ffd194")))
+            .colors(intArrayOf(Color.parseColor(Constants.LOGIN_GRADIENT_COLOR1), Color.parseColor(Constants.LOGIN_GRADIENT_COLOR2)))
             .on(header)
 
         login.setOnClickListener {
