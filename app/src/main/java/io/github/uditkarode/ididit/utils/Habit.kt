14f166bdeb3d7d946e6a89ea121686a1bfddff6d
Udit Karode <udit.karode@gmail.com>
2019-01-03 15:51:34 +0530

    Habit: Add HabitStatus enumeration
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/Habit.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Habit.kt
@@ -2,4 +2,6 @@ package io.github.uditkarode.ididit.utils
 
 import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
 
+enum class HabitStatus {COMPLETED, FAILED, NOT_MARKED}
+
 class Habit(name: String, buttonType: List<Buttons>): ExpandableGroup<Buttons>(name, buttonType)
