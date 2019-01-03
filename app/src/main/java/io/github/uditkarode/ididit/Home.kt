86e0ff8975c1bca3e22d088d9109774a9591ede5
Udit Karode <udit.karode@gmail.com>
2019-01-03 16:06:36 +0530

    Home: Add function to obtain [MutableHabitsList]s
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/Home.kt
+++ app/src/main/java/io/github/uditkarode/ididit/Home.kt
@@ -5,13 +5,16 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.revely.gradient.RevelyGradient
-import io.github.uditkarode.ididit.adapters.ResolutionAdapter
-import io.github.uditkarode.ididit.utils.BABDrawer
+import io.github.uditkarode.ididit.adapters.ExpandableAdapter
+import io.github.uditkarode.ididit.utils.*
 import kotlinx.android.synthetic.main.activity_home.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Home : AppCompatActivity() {
 
@@ -20,12 +23,26 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(bottombar)
 
+        // @todo: remove after UI testing //
+        val habits = ArrayList<String>()
+
+        habits.add("Dab everyday")
+        habits.add("Press F everyday")
+        habits.add("Fail builds everyday")
+        habits.add("using namespace std; everyday")
+        habits.add("Pray for Harambe everyday")
+        habits.add("Go commit die everyday")
+        habits.add("Jump from clock tower everyday")
+
+        val stats = ArrayList(Arrays.asList(HabitStatus.NOT_MARKED, HabitStatus.COMPLETED, HabitStatus.FAILED, HabitStatus.NOT_MARKED, HabitStatus.NOT_MARKED, HabitStatus.NOT_MARKED, HabitStatus.NOT_MARKED, HabitStatus.NOT_MARKED))
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
         RevelyGradient.linear()
-            .colors(intArrayOf(Color.parseColor("#ddd6f3"), Color.parseColor("#faaca8")))
+            .colors(intArrayOf(Color.parseColor(Constants.HOME_GRADIENT_COLOR1), Color.parseColor(Constants.HOME_GRADIENT_COLOR2)))
             .on(homeheader)
 
-        rv_habits.adapter = ResolutionAdapter(ArrayList())
+        rv_habits.adapter = ExpandableAdapter(getMutableHabitsList(habits), habits, stats)
         rv_habits.layoutManager = LinearLayoutManager(this@Home)
     }
 
@@ -46,5 +63,12 @@ class Home : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    private fun getMutableHabitsList(ar: ArrayList<String>): List<Habit> {
+        val tmp = Arrays.asList(Buttons())
+        val ret = ArrayList(Arrays.asList(Habit(ar[0], tmp)))
+        for(i in 1 until ar.size) ret.add(Habit(ar[i], tmp))
+        return ret
     }
 }
