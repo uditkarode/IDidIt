859a6972f571611d99832356835a6e58cba73a43
Udit Karode <udit.karode@gmail.com>
2019-01-03 16:03:44 +0530

    adapters: ExpandableAdapter: add status based implementation
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/adapters/ExpandableAdapter.kt
+++ app/src/main/java/io/github/uditkarode/ididit/adapters/ExpandableAdapter.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import co.revely.gradient.RevelyGradient
@@ -12,6 +13,9 @@ import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
 import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder
 import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder
 import io.github.uditkarode.ididit.R
+import io.github.uditkarode.ididit.utils.Constants
+import io.github.uditkarode.ididit.utils.HabitStatus
+import io.github.uditkarode.ididit.utils.HabitStatus.*
 
 class HabitViewHolder(itemView: View) : GroupViewHolder(itemView) {
     var expanded = true
@@ -29,27 +33,46 @@ class HabitViewHolder(itemView: View) : GroupViewHolder(itemView) {
     }
 }
 
-class ButtonsViewHolder(itemView: View) : ChildViewHolder(itemView)
+class ButtonsViewHolder(itemView: View) : ChildViewHolder(itemView) {
+    val bCompleted: Button = itemView.findViewById(R.id.completed)
+    val bFailed: Button = itemView.findViewById(R.id.failed)
+}
 
-class ExpandableAdapter(habits: List<ExpandableGroup<*>>, val str: ArrayList<String>) :
+class ExpandableAdapter(habits: List<ExpandableGroup<*>>, private val str: ArrayList<String>, private val stats: ArrayList<HabitStatus>):
     ExpandableRecyclerViewAdapter<HabitViewHolder, ButtonsViewHolder>(habits) {
 
     override fun onCreateGroupViewHolder(parent: ViewGroup, viewType: Int) =
-        HabitViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_habit, parent, false))
+        HabitViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_habit, parent, false))
 
 
     override fun onCreateChildViewHolder(parent: ViewGroup, viewType: Int) =
-        ButtonsViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_buttons, parent, false))
+        ButtonsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_buttons, parent, false))
 
 
     override fun onBindChildViewHolder(holder: ButtonsViewHolder, flatPosition: Int, group: ExpandableGroup<*>, childIndex: Int) {
+        when(stats[flatPosition]){
+            COMPLETED -> {
+                holder.bFailed.setBackgroundColor(Color.parseColor(Constants.COLOR_DISABLED))
+                holder.bCompleted.setBackgroundColor(Color.parseColor(Constants.POSITIVE_BUTTON_COLOR))
+            }
+
+            FAILED -> {
+                holder.bCompleted.setBackgroundColor(Color.parseColor(Constants.COLOR_DISABLED))
+                holder.bFailed.setBackgroundColor(Color.parseColor(Constants.NEGATIVE_BUTTON_COLOR))
+            }
+
+            NOT_MARKED -> {
+                holder.bCompleted.setBackgroundColor(Color.parseColor(Constants.POSITIVE_BUTTON_COLOR))
+                holder.bFailed.setBackgroundColor(Color.parseColor(Constants.NEGATIVE_BUTTON_COLOR))
+            }
+        }
     }
 
     override fun onBindGroupViewHolder(holder: HabitViewHolder, flatPosition: Int, group: ExpandableGroup<*>) {
         holder.tv.text = str[flatPosition]
 
         RevelyGradient.linear()
-            .colors(intArrayOf(Color.parseColor("#ddd6f3"), Color.parseColor("#70e1f5")))
+            .colors(intArrayOf(Color.parseColor(Constants.HABIT_GRADIENT_COLOR1), Color.parseColor(Constants.HABIT_GRADIENT_COLOR2)))
             .on(holder.tv)
     }
 }
