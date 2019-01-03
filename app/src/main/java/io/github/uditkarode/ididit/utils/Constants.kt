d2f504a1fdfbc6e28d0e546255ed00cadede96c7
Udit Karode <udit.karode@gmail.com>
2019-01-03 16:02:50 +0530

    IDI: Add Constants to a class
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

---
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Constants.kt
@@ -0,0 +1,20 @@
+package io.github.uditkarode.ididit.utils
+
+class Constants {
+    companion object {
+        const val LOGIN_GRADIENT_COLOR1 = "#70e1f5"
+        const val LOGIN_GRADIENT_COLOR2 = "#ffd194"
+
+        const val HOME_GRADIENT_COLOR1 = "#ddd6f3"
+        const val HOME_GRADIENT_COLOR2 = "#faaca8"
+
+        const val HABIT_GRADIENT_COLOR1 = "#ddd6f3"
+        const val HABIT_GRADIENT_COLOR2 = "#70e1f5"
+
+        const val POSITIVE_BUTTON_COLOR = "#43a047"
+        const val NEGATIVE_BUTTON_COLOR = "#b71c1c"
+
+        const val COLOR_DISABLED = "#424242"
+
+    }
+}
