d2ac33c3cb501ea3d9074ab3fe867bf3de51e8ce
Udit Karode <udit.karode@gmail.com>
2019-01-04 16:09:36 +0530

    utils: BABDrawer: navigate to History class
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
@@ -1,22 +1,26 @@
 package io.github.uditkarode.ididit.utils
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import io.github.uditkarode.ididit.History
 import io.github.uditkarode.ididit.R
 import kotlinx.android.synthetic.main.util_bottomdrawer.*
 
 class BABDrawer: BottomSheetDialogFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.util_bottomdrawer, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.util_bottomdrawer, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-
+                R.id.history -> startActivity(Intent(navigation_view.context, History::class.java))
             }
             true
         }
