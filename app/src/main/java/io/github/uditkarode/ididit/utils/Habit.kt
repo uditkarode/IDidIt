4de1d2191923ec0608275072cdcd7b7b00aad20e
Udit Karode <udit.karode@gmail.com>
2019-01-04 16:06:47 +0530

    utils: Habit: remove old dependencies and implementations
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/Habit.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Habit.kt
@@ -1,7 +1,3 @@
 package io.github.uditkarode.ididit.utils
 
-import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
-
 enum class HabitStatus {COMPLETED, FAILED, NOT_MARKED}
-
-class Habit(name: String, buttonType: List<Buttons>): ExpandableGroup<Buttons>(name, buttonType)
