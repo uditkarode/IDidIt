204cef9d2cdbaeedc0a9a1ee552bb98989c9c4db
Udit Karode <udit.karode@gmail.com>
2019-01-06 02:03:34 +0530

    LogReg: integrate with web api
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/LogReg.kt
+++ app/src/main/java/io/github/uditkarode/ididit/LogReg.kt
@@ -6,21 +6,21 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.edit
 import co.revely.gradient.RevelyGradient
 import com.afollestad.materialdialogs.MaterialDialog
 import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.OkHttpResponseAndJSONObjectRequestListener
+import es.dmoral.toasty.Toasty
+import io.github.uditkarode.ididit.models.CredentialFactory
 import io.github.uditkarode.ididit.utils.Constants
 import kotlinx.android.synthetic.main.activity_logreg.*
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
-import com.androidnetworking.error.ANError
-import org.json.JSONArray
-import com.androidnetworking.interfaces.JSONArrayRequestListener
-import com.androidnetworking.interfaces.JSONObjectRequestListener
-import com.androidnetworking.interfaces.StringRequestListener
-import io.github.uditkarode.ididit.models.CredentialFactory
+import okhttp3.Response
 import org.json.JSONObject
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 
 class LogReg : Activity() {
@@ -31,40 +31,91 @@ class LogReg : Activity() {
         AndroidNetworking.initialize(applicationContext)
 
         RevelyGradient.linear()
-            .colors(intArrayOf(Color.parseColor(Constants.LOGIN_GRADIENT_COLOR1), Color.parseColor(Constants.LOGIN_GRADIENT_COLOR2)))
+            .colors(
+                intArrayOf(
+                    Color.parseColor(Constants.LOGIN_GRADIENT_COLOR1),
+                    Color.parseColor(Constants.LOGIN_GRADIENT_COLOR2)
+                )
+            )
             .on(header)
 
         login.setOnClickListener {
             val cLogin = CredentialFactory(user.text.toString(), pass.text.toString()).makeCredentials()
-            if(cLogin.isErrorFree()){
+            if (cLogin.isErrorFree()) {
                 initializeLoading()
                 AndroidNetworking.post(Constants.BASE_URL + Constants.LOGIN_ENDPOINT)
                     .addHeaders("Content-Type", "application/json")
                     .addJSONObjectBody(JSONObject().put("username", cLogin.username).put("password", cLogin.password))
                     .build()
-                    .getAsJSONObject(object : JSONObjectRequestListener {
-                        override fun onResponse(response: JSONObject) {
-                            Log.e("GOTRESPONSE", response.toString())
+                    .getAsOkHttpResponseAndJSONObject(object : OkHttpResponseAndJSONObjectRequestListener {
+                        override fun onResponse(okhttpResponse: Response, response: JSONObject) {
+                            Toasty.success(this@LogReg, "Success!", Toast.LENGTH_SHORT, true).show()
+                            getSharedPreferences("account", 0).edit {
+                                putString("token", okhttpResponse.header("X-Auth-Token"))
+                                putString("username", cLogin.username)
+                            }
+                            loader_logreg.animate().alpha(0f).setDuration(100).start()
+                            startActivity(Intent(this@LogReg, Home::class.java)) ; finish()
                         }
 
                         override fun onError(error: ANError) {
-                            if(JSONObject(error.errorBody.toString()).getString("message") == "User is not registered"){
-                                MaterialDialog(this@LogReg).show {
-                                    title(text = "Login Failed")
-                                    message(text = "Incorrect username/password!")
+                            MaterialDialog(this@LogReg).show {
+                                title(text = "Login Failed")
+                                message(text = JSONObject(error.errorBody.toString()).getString("message"))
+                                positiveButton(text="Okay")
+                                positiveButton {
+                                    stopLoading()
                                 }
                             }
                         }
                     }
                     )
             } else {
-                Log.e("GOTERROR", cLogin.errors + " ")
+                MaterialDialog(this@LogReg).show {
+                    title(text = "Login Failed")
+                    message(text = cLogin.errors)
+                    positiveButton(text="Okay")
+                }
+            }
+        }
 
+        register.setOnClickListener {
+            val cLogin = CredentialFactory(user.text.toString(), pass.text.toString()).makeCredentials()
+            if (cLogin.isErrorFree()) {
+                initializeLoading()
+                AndroidNetworking.post(Constants.BASE_URL + Constants.SIGN_UP_ENDPOINT)
+                    .addHeaders("Content-Type", "application/json")
+                    .addJSONObjectBody(JSONObject().put("username", cLogin.username).put("password", cLogin.password))
+                    .build()
+                    .getAsOkHttpResponseAndJSONObject(object : OkHttpResponseAndJSONObjectRequestListener {
+                        override fun onResponse(okhttpResponse: Response, response: JSONObject) {
+                            Toasty.success(this@LogReg, "Registration successful!", Toast.LENGTH_SHORT, true).show()
+                            login.performClick()
+                        }
+
+                        override fun onError(error: ANError) {
+                            MaterialDialog(this@LogReg).show {
+                                title(text = "Register Failed")
+                                message(text = JSONObject(error.errorBody.toString()).getString("message"))
+                                positiveButton(text="Okay")
+                                positiveButton {
+                                    stopLoading()
+                                }
+                            }
+                        }
+                    }
+                    )
+            } else {
+                MaterialDialog(this@LogReg).show {
+                    title(text = "Login Failed")
+                    message(text = cLogin.errors)
+                    positiveButton(text="Okay")
+                }
             }
         }
     }
 
-    private fun initializeLoading(){
+    private fun initializeLoading() {
         logregbutts.animate().setDuration(200).alpha(0f).start()
         Handler().postDelayed({
             logregbutts.visibility = View.GONE
@@ -74,7 +125,7 @@ class LogReg : Activity() {
         }, 200)
     }
 
-    private fun stopLoading(){
+    private fun stopLoading() {
         loader_logreg.animate().setDuration(200).alpha(0f).start()
         Handler().postDelayed({
             loader_logreg.visibility = View.GONE
