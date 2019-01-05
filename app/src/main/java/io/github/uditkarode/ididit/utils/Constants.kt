73b174cf32411926f900c3b99eff8c9154449b4b
Udit Karode <udit.karode@gmail.com>
2019-01-05 12:16:26 +0530

    Constants: move incorrect comment directive
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/Constants.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Constants.kt
@@ -4,8 +4,8 @@ class Constants {
     companion object {
         const val BASE_URL = "https://vast-everglades-91661.herokuapp.com/api/"
         const val SIGN_UP_ENDPOINT = "user/sign-up/"
-        const val CHECK_USERNAME_ENDPOINT = "user/username/"
-        const val LOGIN_ENDPOINT = "user/login/" //Append username to check for after this
+        const val CHECK_USERNAME_ENDPOINT = "user/username/" //Append username to check for after this
+        const val LOGIN_ENDPOINT = "user/login/"
         const val ADD_RESOLUTION_ENDPOINT = "resolution/save/"
         const val GET_RESOLUTIONS_ENDPOINT = "resolution/"
         const val DELETE_RESOLUTION_ENDPOINT = "resolution/delete/"
