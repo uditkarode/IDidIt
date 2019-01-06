4a20cd4d8026d968ee6909c028046571cc8d51a8
Udit Karode <udit.karode@gmail.com>
2019-01-06 16:55:00 +0530

    utils: BABDrawer: set dynamic arguments from bundle
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.github.uditkarode.ididit.History
 import io.github.uditkarode.ididit.R
@@ -14,6 +15,12 @@ class BABDrawer: BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.util_bottomdrawer, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textView).text = arguments?.getString("userName")
+        view.findViewById<TextView>(R.id.textView2).text = arguments?.getString("joinDate")
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
