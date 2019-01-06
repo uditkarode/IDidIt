51256d3fc8fdc3276a012f84d2fe2a3ad52ab779
Udit Karode <udit.karode@gmail.com>
2019-01-06 17:37:14 +0530

    BABDrawer: add log out implementation
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/BABDrawer.kt
@@ -1,13 +1,17 @@
 package io.github.uditkarode.ididit.utils
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import es.dmoral.toasty.Toasty
 import io.github.uditkarode.ididit.History
+import io.github.uditkarode.ididit.LogReg
 import io.github.uditkarode.ididit.R
 import kotlinx.android.synthetic.main.util_bottomdrawer.*
 
@@ -28,6 +32,11 @@ class BABDrawer: BottomSheetDialogFragment() {
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.history -> startActivity(Intent(navigation_view.context, History::class.java))
+                R.id.logout -> {
+                    activity?.getSharedPreferences("accounts", 0)?.edit()?.clear()?.apply()
+                    Toasty.success(activity as Context, "Log out successful!", Toast.LENGTH_SHORT, true).show()
+                    startActivity(Intent(activity, LogReg::class.java))
+                }
             }
             true
         }
