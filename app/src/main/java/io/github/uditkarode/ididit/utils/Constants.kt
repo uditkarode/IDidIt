4561079f9752e69ec142c9ff72a25d1321e76e11
Udit Karode <udit.karode@gmail.com>
2019-01-06 17:38:04 +0530

    Constants: comment out unneeded property
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/utils/Constants.kt
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Constants.kt
@@ -3,12 +3,12 @@ package io.github.uditkarode.ididit.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://vast-everglades-91661.herokuapp.com/api/"
-        const val SIGN_UP_ENDPOINT = "user/sign-up/"
-        const val CHECK_USERNAME_ENDPOINT = "user/username/" //Append username to check for after this
-        const val LOGIN_ENDPOINT = "user/login/"
-        const val ADD_RESOLUTION_ENDPOINT = "resolution/save/"
-        const val GET_RESOLUTIONS_ENDPOINT = "resolution/"
-        const val DELETE_RESOLUTION_ENDPOINT = "resolution/delete/"
+        const val SIGN_UP_ENDPOINT = "user/sign-up"
+        //const val CHECK_USERNAME_ENDPOINT = "user/username/" //Append username to check for after this
+        const val LOGIN_ENDPOINT = "user/login"
+        const val ADD_RESOLUTION_ENDPOINT = "resolution/save"
+        const val GET_RESOLUTIONS_ENDPOINT = "resolution"
+        const val DELETE_RESOLUTION_ENDPOINT = "resolution/delete"
         const val UPDATE_STATUS_ENDPOINT = "/"
 
         const val LOGIN_GRADIENT_COLOR1 = "#70E1f5"
