5878c4af02d832d3a0f0d2eccc84ac693a2e73c4
Udit Karode <udit.karode@gmail.com>
2019-03-22 04:52:09 +0530

    [lot of stuff, i cant type that many commits]
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/AboutUs.kt
+++ app/src/main/java/io/github/uditkarode/ididit/AboutUs.kt
@@ -1,10 +1,8 @@
 package io.github.uditkarode.ididit
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import co.revely.gradient.RevelyGradient
 import io.github.uditkarode.ididit.utils.Constants
@@ -25,37 +23,6 @@ class AboutUs: AppCompatActivity(){
                 )
             )
             .on(about_header)
-
-        val color1 = Color.parseColor(Constants.LOGIN_GRADIENT_COLOR1)
-        val color2 = Color.parseColor(Constants.LOGIN_GRADIENT_COLOR2)
-
-        val valueAnimator = ValueAnimator.ofFloat(0f, 360f)
-        valueAnimator.duration = 150
-        valueAnimator.repeatCount = ValueAnimator.INFINITE
-        valueAnimator.interpolator = LinearInterpolator()
-
-        RevelyGradient.linear()
-            .colors(intArrayOf(color1, color2, color1))
-            .animate(valueAnimator) { _valueAnimator, _gradientDrawable ->
-                _gradientDrawable.angle = _valueAnimator.animatedValue as Float
-            }
-            .on(udit)
-
-        RevelyGradient.linear()
-            .colors(intArrayOf(color1, color2, color1))
-            .animate(valueAnimator) { _valueAnimator, _gradientDrawable ->
-                _gradientDrawable.angle = _valueAnimator.animatedValue as Float
-            }
-            .on(karthik)
-
-        RevelyGradient.linear()
-            .colors(intArrayOf(color1, color2, color1))
-            .animate(valueAnimator) { _valueAnimator, _gradientDrawable ->
-                _gradientDrawable.angle = _valueAnimator.animatedValue as Float
-            }
-            .on(himanshu)
-
-        valueAnimator.start()
     }
 
     override fun attachBaseContext(newBase: Context) {
