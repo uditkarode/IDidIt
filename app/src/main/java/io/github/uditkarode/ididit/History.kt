2f013a299009054bd4c317cbf30d211fdc623703
Udit Karode <udit.karode@gmail.com>
2019-03-22 02:18:46 +0530

    [lot of stuff, i cant type that many commits]
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/ididit/History.kt
+++ app/src/main/java/io/github/uditkarode/ididit/History.kt
@@ -3,9 +3,20 @@ package io.github.uditkarode.ididit
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.materialdialogs.MaterialDialog
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONArrayRequestListener
 import io.github.uditkarode.ididit.adapters.HistoryAdapter
+import io.github.uditkarode.ididit.models.Habit
+import io.github.uditkarode.ididit.utils.Constants
+import io.github.uditkarode.ididit.utils.HabitStatistics
 import kotlinx.android.synthetic.main.activity_history.*
+import org.json.JSONArray
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 class History: Activity(){
@@ -14,13 +25,61 @@ class History: Activity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
 
-        val h = ArrayList<String>()
-        h.add("Heya")
-        val k = ArrayList<Int>()
-        k.add(2)
+        val dateArray = java.util.ArrayList<Int>()
+        val dayArray = java.util.ArrayList<String>()
+        val hsArray = ArrayList<HabitStatistics>()
 
-        rvRoot.adapter = HistoryAdapter(k, h)
-        rvRoot.layoutManager = LinearLayoutManager(this@History)
+        historyDataIsLoading()
+
+        AndroidNetworking.get(Constants.BASE_URL + Constants.GET_HISTORY_ENDPOINT)
+            .addHeaders("Authorisation", getSharedPreferences("account", 0).getString("token", "ERROR"))
+            .build()
+            .getAsJSONArray(object: JSONArrayRequestListener {
+                override fun onResponse(response: JSONArray?) {
+                    if(hsArray.isNotEmpty()) hsArray.clear()
+                    for(i in 0 until response?.length()!!) {
+                        val tmpObj = response.getJSONObject(i)
+
+                        dateArray.add(tmpObj.getString("date").toInt())
+                        dayArray.add(tmpObj.getString("day"))
+                        hsArray.add(HabitStatistics(tmpObj.getString("completed").toInt(),
+                            tmpObj.getString("failed").toInt(), tmpObj.getString("not_marked").toInt()))
+
+                    }
+                    historyDataHasLoaded()
+                }
+
+                override fun onError(anError: ANError?) {
+                    MaterialDialog(this@History).show {
+                        title(text = "Data retrieval failed!")
+                        message(text = anError?.errorBody)
+                        positiveButton(text="Okay")
+                        positiveButton {
+                            finish()
+                        }
+                    }
+                }
+            })
+
+        history_rv.adapter = HistoryAdapter(dateArray, dayArray, hsArray)
+        history_rv.layoutManager = LinearLayoutManager(this@History)
+    }
+
+    private fun historyDataIsLoading(){
+        Handler().postDelayed({
+            history_loader.visibility = View.VISIBLE
+            history_loader.alpha = 0f
+            history_loader.animate().alpha(1f).setDuration(150).start()
+        }, 150)
+    }
+
+    private fun historyDataHasLoaded(){
+        history_loader.animate().alpha(0f).setDuration(250).start()
+        Handler().postDelayed({
+            history_loader.visibility = View.GONE
+            history_rv.alpha = 0f
+            history_rv.animate().alpha(1f).setDuration(150).start()
+        }, 150)
     }
 
     override fun attachBaseContext(newBase: Context) {
---
+++ app/src/main/java/io/github/uditkarode/ididit/utils/Habit_Utils.kt
@@ -0,0 +1,5 @@
+package io.github.uditkarode.ididit.utils
+
+enum class HabitStatus {COMPLETED, FAILED, NOT_MARKED}
+
+class HabitStatistics(val completed: Int, val failed: Int, val notMarked: Int)
